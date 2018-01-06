8a362e7a83615f44998d92529a316c430517ced4
alan bount <alan@zeroasterisk.com>
2018-01-05 23:25:23 -0700

    Add BotBits get_all_start_yz +tests
    also refactored to only allow distance out to tile_count, not always 7

--- lib/bot_bits/bot_bits.ex
+++ lib/bot_bits/bot_bits.ex
@@ -7,17 +7,18 @@ defmodule Wordza.BotBits do
   - start_yx_possible?
   - build_all_words_for_letters
   """
- 
+  alias Wordza.GameBoard
+
   @doc """
   Is the y+x a possible start on the board?
   - must be an unplayed, valid square
   - must be within 7 of a played letter
   """
-  def start_yx_possible?(board, y, x) do
+  def start_yx_possible?(board, y, x, tiles_in_tray) do
     !!(
       board
       |> start_yx_playable?(y, x)
-      |> start_yx_within7?(y, x)
+      |> start_yx_within_tile_count?(y, x, Enum.count(tiles_in_tray))
     )
   end
 
@@ -36,24 +37,26 @@ defmodule Wordza.BotBits do
   A start must within 7 of an already played spot
   in either x or y
   """
-  def start_yx_within7?(false, _y, _x), do: false
-  def start_yx_within7?(board, y, x) do
+  def start_yx_within_tile_count?(false, _y, _x, _t), do: false
+  def start_yx_within_tile_count?(_board, _y, _x, 0), do: false
+  def start_yx_within_tile_count?(board, y, x, tile_count) do
+    [] |> get_xy_played(board, y, x, tile_count) |> Enum.any?()
+  end
+
+  @doc """
+  Get a list of "is tile played" for all placements
+  down and right of a given y+x coordinate
+  for the tile_count amount (usually 7)
+
+  This is used to see if a y+x is "within reach" of a played square
+  """
+  def get_xy_played(acc, _board, _y, _x, 0), do: acc
+  def get_xy_played(acc, board, y, x, tile_count) do
     [
-      yx_played?(board, y, x + 1),
-      yx_played?(board, y, x + 2),
-      yx_played?(board, y, x + 3),
-      yx_played?(board, y, x + 4),
-      yx_played?(board, y, x + 5),
-      yx_played?(board, y, x + 6),
-      yx_played?(board, y, x + 7),
-      yx_played?(board, y + 1, x),
-      yx_played?(board, y + 2, x),
-      yx_played?(board, y + 3, x),
-      yx_played?(board, y + 4, x),
-      yx_played?(board, y + 5, x),
-      yx_played?(board, y + 6, x),
-      yx_played?(board, y + 7, x),
-    ] |> Enum.any?()
+      yx_played?(board, y + tile_count, x),
+      yx_played?(board, y, x + tile_count)
+      | acc
+    ] |> get_xy_played(board, y, x, tile_count - 1)
   end
 
   @doc """
@@ -61,7 +64,7 @@ defmodule Wordza.BotBits do
   It must be a valid place, and have no letter already
   """
   def yx_playable?(board, y, x) do
-    is_nil(board[y][x][:letter]) and !is_nil(board[y][x]) 
+    is_nil(board[y][x][:letter]) and !is_nil(board[y][x])
   end
 
   @doc """
@@ -69,7 +72,35 @@ defmodule Wordza.BotBits do
   It must be a valid place, and have a letter already
   """
   def yx_played?(board, y, x) do
-    is_bitstring(board[y][x][:letter]) 
+    is_bitstring(board[y][x][:letter])
+  end
+
+  @doc """
+  Extract all possible start yx squares from a board
+
+  ## Examples
+
+      iex> board = Wordza.GameBoard.create(:mock)
+      iex> tiles = Wordza.GameTiles.add([], "a", 1, 7)
+      iex> Wordza.BotBits.get_all_start_yx(board, tiles)
+      []
+
+      iex> letters_yx = [["A", 2, 2]]
+      iex> board = Wordza.GameBoard.create(:mock) |> Wordza.GameBoard.add_letters_xy(letters_yx)
+      iex> tiles = Wordza.GameTiles.add([], "a", 1, 7)
+      iex> Wordza.BotBits.get_all_start_yx(board, tiles)
+      [[0, 2], [1, 2], [2, 0], [2, 1]]
+
+      iex> letters_yx = [["A", 2, 2], ["A", 2, 3], ["A", 2, 4]]
+      iex> board = Wordza.GameBoard.create(:mock) |> Wordza.GameBoard.add_letters_xy(letters_yx)
+      iex> tiles = Wordza.GameTiles.add([], "a", 1, 7)
+      iex> Wordza.BotBits.get_all_start_yx(board, tiles)
+      [[0, 2], [0, 3], [0, 4], [1, 2], [1, 3], [1, 4], [2, 0], [2, 1]]
+  """
+  def get_all_start_yx(board, tiles_in_tray) do
+    board
+    |> GameBoard.to_yx_list()
+    |> Enum.filter(fn([y, x]) -> start_yx_possible?(board, y, x, tiles_in_tray) end)
   end
 
 end
--- test/bot_bits/bot_bits_test.exs
+++ test/bot_bits/bot_bits_test.exs
@@ -3,35 +3,49 @@ defmodule BotBitsTest do
   doctest Wordza.BotBits
   alias Wordza.BotBits
   alias Wordza.GameBoard
+  alias Wordza.GameTiles
 
   test "start_yx_playable is false, if already played square" do
     board = GameBoard.create(:scrabble)
-    |> put_in([9, 7, :letter], "a") 
-    |> put_in([9, 8, :letter], "a") 
-    |> put_in([9, 9, :letter], "a") 
-    assert BotBits.start_yx_possible?(board, 9, 9) == false
+    |> put_in([9, 7, :letter], "a")
+    |> put_in([9, 8, :letter], "a")
+    |> put_in([9, 9, :letter], "a")
+    tiles = GameTiles.add([], "a", 1, 7)
+    assert BotBits.start_yx_possible?(board, 9, 9, tiles) == false
   end
-
 
   test "start_yx_playable is false, if no overlap within 7 tiles of played word" do
     board = GameBoard.create(:scrabble)
-    |> put_in([9, 7, :letter], "a") 
-    |> put_in([9, 8, :letter], "a") 
-    |> put_in([9, 9, :letter], "a") 
+    |> put_in([9, 7, :letter], "a")
+    |> put_in([9, 8, :letter], "a")
+    |> put_in([9, 9, :letter], "a")
+    tiles = GameTiles.add([], "a", 1, 7)
     # totally alone
-    assert BotBits.start_yx_possible?(board, 0, 0) == false
+    assert BotBits.start_yx_possible?(board, 0, 0, tiles) == false
     # too far above
-    assert BotBits.start_yx_possible?(board, 0, 8) == false
+    assert BotBits.start_yx_possible?(board, 0, 8, tiles) == false
     # below a valid tile (not a valid start)
-    assert BotBits.start_yx_possible?(board, 10, 8) == false
+    assert BotBits.start_yx_possible?(board, 10, 8, tiles) == false
+  end
+
+  test "start_yx_playable is false, if no overlap within 3 tiles of played word" do
+    board = GameBoard.create(:scrabble)
+    |> put_in([9, 7, :letter], "a")
+    |> put_in([9, 8, :letter], "a")
+    |> put_in([9, 9, :letter], "a")
+    tiles = GameTiles.add([], "a", 1, 3)
+    # 4 square above, but only 3 in tray
+    assert BotBits.start_yx_possible?(board, 4, 7, tiles) == false
   end
 
   test "start_yx_playable is true, if valid" do
     board = GameBoard.create(:scrabble)
-    |> put_in([9, 7, :letter], "a") 
-    |> put_in([9, 8, :letter], "a") 
-    |> put_in([9, 9, :letter], "a") 
-    assert BotBits.start_yx_possible?(board, 7, 7) == true
+    |> put_in([9, 7, :letter], "a")
+    |> put_in([9, 8, :letter], "a")
+    |> put_in([9, 9, :letter], "a")
+    tiles = GameTiles.add([], "a", 1, 7)
+    # 7 square above, 7 in tray
+    assert BotBits.start_yx_possible?(board, 2, 7, tiles) == true
   end
 
 end
